b650a7e08820192aaed175d6c918e0f0767690f4
Nico <jumpingpxl@gmail.com>
2023-03-09 19:33:44 +0100

    bump labygradle version, remove support for 1.19.4-pre3, add support for 1.19.4-rc1

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,7 @@ labyMod {
                 "1.18.2",
                 "1.19.2",
                 "1.19.3",
-                "1.19.4-pre3"
+                "1.19.4-rc1"
         ) { version, provider ->
             configureRun(provider, version)
         }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.18"
+    val labyGradlePluginVersion = "0.3.19"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
