719e7fc14fd56bab8cd4f0aa9fa8ee3da38b5d30
Nico <jumpingpxl@gmail.com>
2023-06-01 13:13:05 +0200

    bump labygradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.3.23"
+    val labyGradlePluginVersion = "0.3.25"
     plugins {
         id("net.labymod.gradle") version (labyGradlePluginVersion)
     }
