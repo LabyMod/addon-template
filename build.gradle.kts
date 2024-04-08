ac18c81d9fee1e50704be437f93d491739576f67
MuffinTime <50742161+RealMuffinTime@users.noreply.github.com>
2024-04-08 15:26:48 +0200

    unifiy version vars in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.example"
-version = "1.0.0"
+version = System.getenv().getOrDefault("VERSION", "1.0.0")
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
 
@@ -17,7 +17,7 @@ labyMod {
         author = "Example Author"
         description = "Example Description"
         minecraftVersion = "*"
-        version = System.getenv().getOrDefault("VERSION", "0.0.1")
+        version = getVersion().toString()
     }
 
     minecraft {
