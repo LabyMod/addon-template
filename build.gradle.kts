be83b802d7fb800c03b4e5711d56c5e29bc3a934
EinsJustinnn <z0ck3r98hd@gmail.com>
2024-04-23 18:40:54 +0200

    add 1.20.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,8 @@ labyMod {
                 "1.19.4",
                 "1.20.1",
                 "1.20.2",
-                "1.20.4"
+                "1.20.4",
+                "1.20.5"
         ) { version, provider ->
             configureRun(provider, version)
         }
