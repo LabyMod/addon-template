09fc2310fd3a019e83a64fccd513ec8d725dde4b
Christian <r0bby@gmx.net>
2024-09-02 12:51:16 +0200

    Update build scripts and dependencies to latest LabyMod versions
    Switched to the latest 'net.labymod.labygradle' plugin and updated Gradle Wrapper to version 8.10. Consolidated Minecraft version declarations and simplified subproject configurations. Adjusted annotation processor configurations and added missing properties for better consistency.

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,18 +1,9 @@
-version = "0.1.0"
-
-plugins {
-    id("java-library")
-}
+import net.labymod.labygradle.common.extension.LabyModAnnotationProcessorExtension.ReferenceType
 
 dependencies {
     labyApi("api")
 }
 
-labyModProcessor {
-    referenceType = net.labymod.gradle.core.processor.ReferenceType.INTERFACE
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+labyModAnnotationProcessor {
+    referenceType = ReferenceType.INTERFACE
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,18 +1,9 @@
-version = "0.1.0"
-
-plugins {
-    id("java-library")
-}
+import net.labymod.labygradle.common.extension.LabyModAnnotationProcessorExtension.ReferenceType
 
 dependencies {
     api(project(":api"))
 }
 
-labyModProcessor {
-    referenceType = net.labymod.gradle.core.processor.ReferenceType.DEFAULT
-}
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+labyModAnnotationProcessor {
+    referenceType = ReferenceType.DEFAULT
 }
