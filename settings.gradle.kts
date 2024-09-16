e712c1de6611fa1ffef8602b79b68fd3abcda901
Christian <r0bby@gmx.net>
2024-09-16 14:36:19 +0200

    bump labygradle to 0.5.4

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.3"
+    val labyGradlePluginVersion = "0.5.4"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
