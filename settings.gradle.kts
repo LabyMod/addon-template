b805c2df546ae3d063ca1b8ed9ab69a30e6f1383
RappyTV <rappytv@rappytv.com>
2026-01-09 00:18:16 +0100

    chore: Ignore access widener index & use stable laby gradle version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.6.0-SNAPSHOT"
+    val labyGradlePluginVersion = "0.6.0"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
