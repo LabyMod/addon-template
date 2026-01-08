8d4ca14e9b0b97e235f9cf93116bd69e8081126d
RappyTV <rappytv@rappytv.com>
2026-01-08 20:01:36 +0100

    chore: Keep template up to date * Update supported versions * Use Java 25 and Gradle 9.2.1 * Bump LabyGradle * Ignore new resource index file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.5.9"
+    val labyGradlePluginVersion = "0.6.0-SNAPSHOT"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
