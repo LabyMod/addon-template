848ca723e34acabc2adeeac4d90fcc0bcbf0a346
Christian Schelle <36857475+R0bbyYT@users.noreply.github.com>
2026-03-02 10:23:46 +0100

    Update LabyGradle version to 0.6.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.name = "labymod4-addon-template"
 
 pluginManagement {
-    val labyGradlePluginVersion = "0.6.1"
+    val labyGradlePluginVersion = "0.6.2"
     buildscript {
         repositories {
             maven("https://dist.labymod.net/api/v1/maven/release/")
